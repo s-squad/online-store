f51089918e27e47a8ab37747e225746efa0723a3
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2022-12-16 16:41:01 +0500

    feat: created basis structure component Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Logo.module.scss';
+
+export const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <Link to={'/'}>
+        <img
+          src={process.env.PUBLIC_URL + 'logo.svg'}
+          width='150'
+          height='50'
+          alt='Logo text'
+        />
+      </Link>
+    </div>
+  );
+};
