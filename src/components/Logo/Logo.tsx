26b74f0917ad650c4ac310bd17038509f4218a0a
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2022-12-19 15:40:27 +0500

    refactor: update Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LogoIcons from '../../assets/icons/logo.svg';
 import styles from './Logo.module.scss';
 
 export const Logo = () => {
   return (
     <div className={styles.logo}>
       <Link to={'/'}>
-        <img
-          src={process.env.PUBLIC_URL + 'logo.svg'}
-          width='150'
-          height='50'
-          alt='Logo text'
-        />
+        <img src={LogoIcons} width='150' height='50' alt='Logo text' />
       </Link>
     </div>
   );
