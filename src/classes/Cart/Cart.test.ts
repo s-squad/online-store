2316f8e81c6a1c5f07c8cd2c364f362b0a2420d8
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2022-12-20 13:51:23 +0500

    refactor: apply eslint for Cart.test.ts

--- src/classes/Cart/Cart.test.ts
+++ src/classes/Cart/Cart.test.ts
@@ -7,7 +7,7 @@ describe('сlass Cart', () => {
     price: 22.3,
     description:
       'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: "men's clothing",
+    category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
     rating: {
       rate: 4.1,
@@ -20,7 +20,7 @@ describe('сlass Cart', () => {
     price: 22.3,
     description:
       'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: "men's clothing",
+    category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
     rating: {
       rate: 4.1,
