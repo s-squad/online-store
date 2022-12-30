c96db7745862c0e9ed3481a9e0974535c982b894
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-12-29 20:14:39 -0500

    feat: add layout for Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import { Htag } from '../../components';
+import { CustomContainer } from '../../containers/CustomContainer';
 import styles from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => {
-  return <div className={styles.main}>NotFoundPage</div>;
+  return (
+    <section className={styles.main}>
+      <CustomContainer>
+        <Htag tag='h1'>Page not found</Htag>
+        <div className={styles.message}>
+          <p>Sorry! We couldn<span>&apos;</span>t fetch that page.</p>
+          <p>Try searching or go to<Link to={'/'} className={styles.link}>CREATEX<span>&apos;</span>s home page.</Link></p>
+        </div>
+      </CustomContainer>
+    </section>
+  );
 };
