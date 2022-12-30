93d5562dca9431c502699c3bb0ee5ca75680358a
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-12-29 20:16:02 -0500

    style: apply formatter

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -9,8 +9,15 @@ export const NotFoundPage = () => {
       <CustomContainer>
         <Htag tag='h1'>Page not found</Htag>
         <div className={styles.message}>
-          <p>Sorry! We couldn<span>&apos;</span>t fetch that page.</p>
-          <p>Try searching or go to<Link to={'/'} className={styles.link}>CREATEX<span>&apos;</span>s home page.</Link></p>
+          <p>
+            Sorry! We couldn<span>&apos;</span>t fetch that page.
+          </p>
+          <p>
+            Try searching or go to
+            <Link to={'/'} className={styles.link}>
+              CREATEX<span>&apos;</span>s home page.
+            </Link>
+          </p>
         </div>
       </CustomContainer>
     </section>
