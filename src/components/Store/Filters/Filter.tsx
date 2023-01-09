02f440312a7b04533a372262103b62fe8c88e3f5
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-01-10 01:32:32 +0500

    feat: add Store

---
+++ src/components/Store/Filters/Filter.tsx
@@ -0,0 +1,87 @@
+import cn from 'classnames';
+import { useState } from 'react';
+
+import { Accordion, Button } from '../../';
+import { SliderRange } from '../../SliderRange';
+
+import styles from './Filters.module.scss';
+import { FiltersProps } from './Filters.props';
+
+export const Filters = ({
+  brands,
+  categories,
+  handleChange,
+  stockRange,
+  priceRange,
+  resetFilters,
+  copyLink,
+}: FiltersProps) => {
+  const [priceVal, setPriceVal] = useState<number | number[]>(priceRange);
+  const [stockVal, setStockVal] = useState<number | number[]>(stockRange);
+
+  const filters = [
+    { title: 'Categoryes', array: categories, filter: 'category' },
+    { title: 'Brands', array: brands, filter: 'brand' },
+  ];
+  const filtersRange = [
+    {
+      title: 'Stock',
+      value: { name: stockVal, initialValue: [0, 100] },
+    },
+    { title: 'Price', value: { name: priceVal, initialValue: [20, 1500] } },
+  ];
+
+  return (
+    <div className={cn(styles.filters)}>
+      <div className={styles.filtersButtons}>
+        <Button onClick={resetFilters}>Reset Filters</Button>
+        <Button onClick={copyLink}>Copy Link</Button>
+      </div>
+      {filters.map((item, id) => (
+        <Accordion key={id} title={item.title}>
+          {item.array.map((checkbox) => {
+            return (
+              <label key={checkbox.name} className={cn(styles.label)}>
+                <input
+                  className={styles.inp}
+                  type='checkbox'
+                  name={checkbox.name}
+                  id=''
+                  onChange={(event) => handleChange(event, item.filter)}
+                />
+                {checkbox.name.replace(checkbox.name[0], checkbox.name[0].toUpperCase())}
+                <span>({checkbox.count})</span>
+              </label>
+            );
+          })}
+        </Accordion>
+      ))}
+      {filtersRange.map((item) => {
+        return (
+          <Accordion key={item.title} title={item.title}>
+            <SliderRange
+              onAfterChange={() => {
+                1;
+              }}
+              sliderValues={
+                Array.isArray(item.value.name) ? item.value.name : item.value.initialValue
+              }
+              handleChange={
+                item.title === 'Price'
+                  ? (value) => {
+                      setPriceVal(value);
+                    }
+                  : (value) => {
+                      setStockVal(value);
+                    }
+              }
+              key={item.title}
+              min={item.value.initialValue[0]}
+              max={item.value.initialValue[1]}
+            />
+          </Accordion>
+        );
+      })}
+    </div>
+  );
+};
---
+++ src/components/Store/StoreContainer.tsx
@@ -0,0 +1,233 @@
+import { useEffect, useState, ChangeEvent } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+
+import { Filters } from '../../components/Store/Filters';
+
+import { ReactComponent as GridIcon } from '../../assets/icons/grid.svg';
+import { ReactComponent as ListIcon } from '../../assets/icons/list.svg';
+
+import { IProduct, IResponse, ESorted, IParams } from '../../model';
+
+import styles from './StoreContainer.module.scss';
+import { Button, Select } from '../../components';
+import { getFilters } from '../../utils/getFilters';
+import { CardList } from './CardList';
+import { applyFilters } from '../../utils/applyFilters';
+import { getFiltersRange } from '../../utils/getFiltersRange';
+
+export const StoreContainer = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [layout, setLayout] = useState<string>(searchParams.get('layout') ?? 'grid');
+  const [brands, setBrands] = useState<{ name: string; count: number }[]>([]);
+  const [categories, setCategories] = useState<{ name: string; count: number }[]>([]);
+  const [sortProperty, setSortProperty] = useState('');
+  const [limit, setLimit] = useState<string>(localStorage.getItem('limit') ?? '12');
+  const [toggleVisabilityFilter, setToggleVisabilityFilter] = useState<boolean>(true);
+
+  const params: IParams = {
+    sort: sortProperty,
+    name: searchParams.get('name') || '',
+    brand: searchParams.get('brand') || '',
+    category: searchParams.get('category') || '',
+    descr: searchParams.get('descr') || '',
+    rating: searchParams.get('rating') || '',
+    stock: searchParams.get('rating') || ''
+  };
+
+  const [price, setPrice] = useState<number[]>([20, 1500]);
+  const [stock, setStock] = useState<number[]>([0, 100]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>, title: string) => {
+    let brands = searchParams.get('brand')?.split(',') ?? [];
+    let categoryes = searchParams.get('category')?.split(',') ?? [];
+    if (event.target.checked) {
+      if (title === 'brand') {
+        brands.push(event.target.name);
+      } else {
+        categoryes.push(event.target.name);
+      }
+    } else {
+      if (title === 'brand') {
+        brands = brands.filter((brand) => brand !== event.target.name);
+      } else {
+        categoryes = categoryes.filter((category) => category !== event.target.name);
+      }
+    }
+    if (brands.length > 0) {
+      searchParams.set('brand', brands.join(','));
+    } else {
+      searchParams.delete('brand');
+    }
+    if (categoryes.length > 0) {
+      searchParams.set('category', categoryes.join(','));
+    } else {
+      searchParams.delete('category');
+    }
+    setSearchParams(searchParams);
+  };
+
+  const [itemOffset, setItemOffset] = useState(0);
+  const pageCount = Math.ceil(applyFilters({ products, params }).length / Number(limit));
+
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * Number(limit)) % applyFilters({ products, params }).length;
+    setItemOffset(newOffset);
+  };
+  const resetFilters = () => setSearchParams({});
+  const copyLink = () => navigator.clipboard.writeText(window.location.href);
+
+  const handleSort = (event: ChangeEvent<HTMLSelectElement>) => {
+    const { target } = event;
+    if (target) {
+      setSortProperty(target.value);
+      setSearchParams({ sort: target.value });
+    }
+  };
+  const [correntItems, setCorrentItems] = useState<IProduct[]>([]);
+  const endOffset = itemOffset + +limit;
+  const changeLayout = () => {
+    if (layout === 'grid') {
+      setLayout('list');
+      setSearchParams({ layout: 'list' });
+    } else {
+      setLayout('grid');
+      setSearchParams({ layout: 'grid' });
+    }
+  };
+
+  useEffect(() => {
+    setBrands(getFilters(correntItems, 'brand'));
+    setCorrentItems(applyFilters({ products: correntItems.slice(itemOffset, endOffset), params }));
+    setCategories(getFilters(correntItems, 'category'));
+    setPrice(getFiltersRange(correntItems, 'price'));
+    setStock(getFiltersRange(correntItems, 'stock'));
+  }, [itemOffset, limit, searchParams]);
+
+  useEffect(() => {
+    const getProductsList = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data: IResponse = await response.json();
+        setProducts(data.products);
+        setBrands(getFilters(data.products, 'brand'));
+        setCategories(getFilters(data.products, 'category'));
+        setCorrentItems(
+          applyFilters({ products: data.products.slice(itemOffset, endOffset), params }),
+        );
+        setPrice(getFiltersRange(data.products, 'price'));
+        setStock(getFiltersRange(data.products, 'stock'));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProductsList();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.settings}>
+        <Button size='large' onClick={() => setToggleVisabilityFilter((prev) => !prev)}>
+          Hide filters
+        </Button>
+        <div className={styles.inputContainer}>
+          Sort by
+          <Select
+            name='sort'
+            value={sortProperty}
+            options={[
+              { value: ESorted.NAME, key: '1', text: 'Name' },
+              { value: ESorted.PRICE_DESC, key: '2', text: 'Price descending' },
+              { value: ESorted.PRICE_ASC, key: '3', text: 'Price ascending' },
+              { value: ESorted.RATING_DESC, key: '4', text: 'Rating descending' },
+              { value: ESorted.RATING_ASC, key: '5', text: 'Rating ascending' },
+              { value: ESorted.STOCK_DESC, key: '6', text: 'Stock ascending' },
+              { value: ESorted.STOCK_ASC, key: '7', text: 'Stock ascending' },
+              { value: ESorted.DISCOUNT_DESC, key: '8', text: 'Discount ascending' },
+              { value: ESorted.DISCOUNT_ASC, key: '9', text: 'Discount ascending' },
+            ]}
+            placeholder='Name'
+            onChange={handleSort}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          Show
+          <label className={styles.label}>
+            <input
+              className={styles.input}
+              type='number'
+              min={1}
+              step={1}
+              readOnly
+              value={limit}
+              onKeyDown={(event) => {
+                if (event.key === 'ArrowUp') {
+                  setLimit((prev) => String(Number(prev) + 1));
+                  localStorage.setItem('limit', String(Number(limit) + 1));
+                } else if (event.key === 'ArrowDown') {
+                  setLimit((prev) => String(Number(prev) - 1));
+                  localStorage.setItem('limit', String(Number(limit) - 1));
+                } else {
+                  return;
+                }
+              }}
+            />
+
+            <button
+              onClick={() => {
+                setLimit((prev) => String(Number(prev) + 1));
+                localStorage.setItem('limit', String(Number(limit) + 1));
+              }}
+              className={styles.arrowTop}
+            >
+              ▲
+            </button>
+            <button
+              onClick={() => {
+                setLimit((prev) => String(Number(prev) - 1));
+                localStorage.setItem('limit', String(Number(limit) - 1));
+              }}
+              className={styles.arrowBottom}
+            >
+              ▼
+            </button>
+          </label>
+          <span>products per page</span>
+        </div>
+        {layout === 'grid' ? (
+          <GridIcon onClick={changeLayout} />
+        ) : (
+          <ListIcon onClick={changeLayout} />
+        )}
+        <ReactPaginate
+          breakLabel='...'
+          nextLabel='>'
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={7}
+          pageCount={pageCount || 1}
+          containerClassName={styles.paginate}
+          previousLabel='<'
+          pageLinkClassName={styles.paginateItem}
+          previousLinkClassName={styles.paginateItem}
+          nextLinkClassName={styles.paginateItem}
+          activeLinkClassName={styles.active}
+        />
+      </div>
+      <div className={styles.flexContainer}>
+        {toggleVisabilityFilter && (
+          <Filters
+            stockRange={stock}
+            priceRange={price}
+            brands={brands}
+            categories={categories}
+            copyLink={copyLink}
+            resetFilters={resetFilters}
+            handleChange={handleChange}
+          />
+        )}
+        <CardList items={correntItems} layout={layout} />
+      </div>
+    </>
+  );
+};
