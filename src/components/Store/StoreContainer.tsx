26b7f2e81ca7bdb853de410075088a38a85d8add
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-06 20:57:51 +0500

    fix: add adaptive for StoreContainer

--- src/components/Store/StoreContainer.tsx
+++ src/components/Store/StoreContainer.tsx
@@ -76,7 +76,7 @@ export const StoreContainer = () => {
     setItemOffset(newOffset);
   };
   const endOffset = itemOffset + +limit;
-  const correntItems = applyFilters({ products: products.slice(itemOffset, endOffset), params })
+  const correntItems = applyFilters({ products: products.slice(itemOffset, endOffset), params });
   const resetFilters = () => setSearchParams({});
   const copyLink = () => navigator.clipboard.writeText(window.location.href);
 
@@ -115,7 +115,6 @@ export const StoreContainer = () => {
         setCategories(getFilters(data.products, 'category'));
         setPrice(getFiltersRange(data.products, 'price'));
         setStock(getFiltersRange(data.products, 'stock'));
-
       } catch (error) {
         console.error(error);
       }
@@ -126,95 +125,103 @@ export const StoreContainer = () => {
   return (
     <>
       <div className={styles.settings}>
-        <Button size='large' onClick={() => setToggleVisabilityFilter((prev) => !prev)}>
+        <Button
+          size='large'
+          onClick={() => setToggleVisabilityFilter((prev) => !prev)}
+          className={styles.hideBtn}
+        >
           Hide filters
         </Button>
-        <div className={styles.inputContainer}>
-          Sort by
-          <Select
-            name='sort'
-            value={sortProperty}
-            options={[
-              { value: ESorted.NAME, key: '1', text: 'Name' },
-              { value: ESorted.PRICE_DESC, key: '2', text: 'Price descending' },
-              { value: ESorted.PRICE_ASC, key: '3', text: 'Price ascending' },
-              { value: ESorted.RATING_DESC, key: '4', text: 'Rating descending' },
-              { value: ESorted.RATING_ASC, key: '5', text: 'Rating ascending' },
-              { value: ESorted.STOCK_DESC, key: '6', text: 'Stock ascending' },
-              { value: ESorted.STOCK_ASC, key: '7', text: 'Stock ascending' },
-              { value: ESorted.DISCOUNT_DESC, key: '8', text: 'Discount ascending' },
-              { value: ESorted.DISCOUNT_ASC, key: '9', text: 'Discount ascending' },
-            ]}
-            placeholder='Name'
-            onChange={handleSort}
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          Show
-          <label className={styles.label}>
-            <input
-              className={styles.input}
-              type='number'
-              min={1}
-              step={1}
-              readOnly
-              value={limit}
-              onKeyDown={(event) => {
-                if (event.key === 'ArrowUp') {
+        <div className={styles.settings__inputs}>
+          <div className={styles.inputContainer}>
+            Sort by
+            <Select
+              name='sort'
+              value={sortProperty}
+              options={[
+                { value: ESorted.NAME, key: '1', text: 'Name' },
+                { value: ESorted.PRICE_DESC, key: '2', text: 'Price descending' },
+                { value: ESorted.PRICE_ASC, key: '3', text: 'Price ascending' },
+                { value: ESorted.RATING_DESC, key: '4', text: 'Rating descending' },
+                { value: ESorted.RATING_ASC, key: '5', text: 'Rating ascending' },
+                { value: ESorted.STOCK_DESC, key: '6', text: 'Stock ascending' },
+                { value: ESorted.STOCK_ASC, key: '7', text: 'Stock ascending' },
+                { value: ESorted.DISCOUNT_DESC, key: '8', text: 'Discount ascending' },
+                { value: ESorted.DISCOUNT_ASC, key: '9', text: 'Discount ascending' },
+              ]}
+              placeholder='Name'
+              onChange={handleSort}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            Show
+            <label className={styles.label}>
+              <input
+                className={styles.input}
+                type='number'
+                min={1}
+                step={1}
+                readOnly
+                value={limit}
+                onKeyDown={(event) => {
+                  if (event.key === 'ArrowUp') {
+                    setLimit((prev) => String(Number(prev) + 1));
+                    localStorage.setItem('limit', String(Number(limit) + 1));
+                  } else if (event.key === 'ArrowDown') {
+                    if (!(Number(limit) <= 1)) {
+                      setLimit((prev) => String(Number(prev) - 1));
+                      localStorage.setItem('limit', String(Number(limit) - 1));
+                    }
+                  } else {
+                    return;
+                  }
+                }}
+              />
+
+              <button
+                onClick={() => {
                   setLimit((prev) => String(Number(prev) + 1));
                   localStorage.setItem('limit', String(Number(limit) + 1));
-                } else if (event.key === 'ArrowDown') {
+                }}
+                className={styles.arrowTop}
+              >
+                ▲
+              </button>
+              <button
+                onClick={() => {
                   if (!(Number(limit) <= 1)) {
                     setLimit((prev) => String(Number(prev) - 1));
                     localStorage.setItem('limit', String(Number(limit) - 1));
                   }
-                } else {
-                  return;
-                }
-              }}
-            />
-
-            <button
-              onClick={() => {
-                setLimit((prev) => String(Number(prev) + 1));
-                localStorage.setItem('limit', String(Number(limit) + 1));
-              }}
-              className={styles.arrowTop}
-            >
-              ▲
-            </button>
-            <button
-              onClick={() => {
-                if (!(Number(limit) <= 1)) {
-                  setLimit((prev) => String(Number(prev) - 1));
-                  localStorage.setItem('limit', String(Number(limit) - 1));
-                }
-              }}
-              className={styles.arrowBottom}
-            >
-              ▼
-            </button>
-          </label>
-          <span>products per page</span>
+                }}
+                className={styles.arrowBottom}
+              >
+                ▼
+              </button>
+            </label>
+            <span>products per page</span>
+          </div>
         </div>
-        {layout === 'grid' ? (
-          <GridIcon onClick={changeLayout} />
-        ) : (
-          <ListIcon onClick={changeLayout} />
-        )}
-        <ReactPaginate
-          breakLabel='...'
-          nextLabel='>'
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={7}
-          pageCount={pageCount || 1}
-          containerClassName={styles.paginate}
-          previousLabel='<'
-          pageLinkClassName={styles.paginateItem}
-          previousLinkClassName={styles.paginateItem}
-          nextLinkClassName={styles.paginateItem}
-          activeLinkClassName={styles.active}
-        />
+        <div className={styles.iconsContainer}>
+          {layout === 'grid' ? (
+            <GridIcon onClick={changeLayout} />
+          ) : (
+            <ListIcon onClick={changeLayout} />
+          )}
+          <ReactPaginate
+            breakLabel='...'
+            nextLabel='>'
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={7}
+            pageCount={pageCount || 1}
+            containerClassName={styles.paginate}
+            previousLabel='<'
+            pageLinkClassName={styles.paginateItem}
+            previousLinkClassName={styles.paginateItem}
+            nextLinkClassName={styles.paginateItem}
+            activeLinkClassName={styles.active}
+          />
+        </div>
       </div>
       <div className={styles.flexContainer}>
         {toggleVisabilityFilter && (
